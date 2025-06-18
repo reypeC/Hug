9515a4cd25f7417068aa8bb2d3a0e39d92f00e7d
Christophe Reype <chris.reype@gmail.com>
2025-06-18 13:57:17 +0200

    Add files via upload

--- C++/EXEC/sim.bat
+++ C++/EXEC/sim.bat
@@ -1,7 +1,6 @@
-
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_athab.txt
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_athabSA.txt
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_pinti.txt
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_pintiSA.txt
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_tetra.txt
-~/EXEC/hugHardcore.exe ~/PARAMS/parameter_tetraSA.txt
+hugHardcore.exe ..\PARAMS\parameter_synthetic.txt
+hugHardcore.exe ..\PARAMS\parameter_syntheticSA.txt
+hugHardcore.exe ..\PARAMS\parameter_mexico.txt
+hugHardcore.exe ..\PARAMS\parameter_mexicoSA.txt
+hugHardcore.exe ..\PARAMS\parameter_athabasca.txt
+hugHardcore.exe ..\PARAMS\parameter_athabascaSA.txt
